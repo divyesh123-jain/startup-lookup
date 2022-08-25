3f676c9d6c77b165b377364dc00d647be6fac440
heyyakash <akashsharma2002@gmail.com>
2022-08-25 16:49:46 +0530

    fixed styling

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() 
+export default function Home(){
+  return(
+    <h1>Hello</h1>
+  )
+} 
   
         
