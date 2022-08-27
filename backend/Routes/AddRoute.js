b6cdcae9c88eb0eccb588c73b993fe2675190deb
heyyakash <akashsharma2002@gmail.com>
2022-08-27 14:21:31 +0530

    fixed entrp login

--- backend/Routes/AddRoute.js
+++ backend/Routes/AddRoute.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const CreateStartup = require('../Middleware/CreateStartup.js');
-const {createTask} = require('../Controllers/AddControllers'); 
+const {createTask,getStartUps} = require('../Controllers/AddControllers'); 
 
-router.route('/').post(CreateStartup,createTask)
+router.route('/').post(CreateStartup,createTask).get(CreateStartup,getStartUps);
 
 module.exports=router;
